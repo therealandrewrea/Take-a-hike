f9d8d95eb9bc02209b282cde5358e902a3ddff3d
Andy Rea <therealandrewrea@gmail.com>
2017-07-12 16:36:47 -0500

    initial commit

---
+++ src/main/java/com/example/takeAhike/TakeAhikeApplication.java
@@ -0,0 +1,12 @@
+package com.example.takeAhike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeAhikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeAhikeApplication.class, args);
+	}
+}
